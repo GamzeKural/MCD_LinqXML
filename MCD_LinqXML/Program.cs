0edaa38d98e432db6adad918783c1501933237db
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-22 17:39:00 +0300

    Add project files.

---
+++ MCD_LinqXML/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MCD_LinqXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument XDoc = new XDocument(
+                new XDeclaration("1.0", "UTF-8","yes"), 
+                new XComment("Yorum satırı"), 
+                new XElement("Personellerim", 
+                new XElement("Personel",
+                new XAttribute("ID","1"), 
+                new XElement("Isim","Ahmet"),
+                new XElement("Soyisim","Koç"), 
+                new XElement("emailadress","ahmetkoc@gmail.com")),
+
+                new XElement("Personel",
+                new XAttribute("ID", "2"),
+                new XElement("Isim", "Gamze"),
+                new XElement("Soyisim", "Kural"),
+                new XElement("emailadress", "gamzekural@gmail.com")))
+                );
+            XDoc.Save(@"C:\XML\Ornek1.xml");
+        }
+    }
+}
